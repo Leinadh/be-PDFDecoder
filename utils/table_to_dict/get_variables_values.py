80b89c33c64eafe0798c36dedb1da92e397364af
Stev <stevhr@gmail.com>
2020-10-17 12:29:54 -0500

    get_variables_values added

---
+++ utils/table_to_dict/get_variables_values.py
@@ -0,0 +1,196 @@
+##!pip install fuzzywuzzy
+##!pip install unidecode
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+import re
+import math
+from collections import Counter
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import unidecode
+from fuzzywuzzy import fuzz
+import pandas as pd
+import json
+from copy import deepcopy
+import difflib
+
+
+def no_space_start_end(x):
+  return re.sub("^[\\s]*|[\\s]*$",'', str(x)) if pd.notnull(x) else ""
+
+def no_space_between(x):
+  return re.sub("[\\s]+",' ', str(x)) if pd.notnull(x) else ""
+
+def upper_case(x):
+  return str(x).upper()
+
+def lower_case(x):
+  return str(x).lower()
+
+def no_alphanum(x):
+  return re.sub("[^\w|^\s]", " ", str(x)) if pd.notnull(x) else ""
+
+def no_numeric(x):
+  return re.sub("\d", " ", str(x)) if pd.notnull(x) else ""
+
+def no_stopwords(x):
+  if pd.notnull(x):
+    stop_words=set(stopwords.words('spanish'))
+    word_tokens = word_tokenize(str(x))
+    return ' '.join([w for w in word_tokens if not w in stop_words]) 
+  else:
+    return ""
+
+def words_unidecode(x):
+  return unidecode.unidecode(str(x)) if pd.notnull(x) else ""
+  
+  
+def is_year(x):
+  if pd.notnull(x):
+    expr = "[\s]+201[0-9]+|^201[0-9]|[\s]201[0-9]$"
+    years = re.findall(expr,str(x))
+
+    return 1 if len(years)>0 else 0
+  else:
+    return 0
+    
+
+def get_year(x):
+  if pd.notnull(x):
+    
+    expr = "[\s]+201[0-9]+|^201[0-9]|[\s]201[0-9]$"
+    years = re.findall(expr,str(x))
+    years = [int(year) for year in years]
+    
+    return np.max(years) if len(years)>0 else 0
+  else:
+    return 0
+    
+    
+
+  
+def processing_text(df):
+  _df = df.copy()
+  _df = _df.applymap(no_space_start_end)
+  _df = _df.applymap(no_space_between)
+  _df = _df.applymap(upper_case)
+  _df = _df.applymap(words_unidecode)
+  
+  _df[_df==""] = np.nan
+
+  return _df
+  
+
+def fuzzy_wuzzy(s_evaluado, s_buscado):
+  return fuzz.partial_ratio(str(s_evaluado),s_buscado)/100 
+
+def sequence_matcher_similarity(s_evaluado, s_buscado):
+  return difflib.SequenceMatcher(None, str(s_evaluado), s_buscado).ratio()
+
+def and_partial_ratio(s_evaluado, s_buscado):
+  similarity = 1
+  for s in s_buscado.split():
+    similarity *= fuzz.partial_ratio(s, str(s_evaluado))/100
+  return similarity
+
+def limited_and_partial_ratio(s_evaluado, s_buscado):
+  s_evaluado = str(s_evaluado)
+  palabras_buscadas = s_buscado.split()
+  palabras_evaluadas = s_evaluado.split()
+  if len(palabras_buscadas) ==  len(palabras_evaluadas):
+    similarity = 1
+    for s in palabras_buscadas:
+      similarity *= fuzz.partial_ratio(s, s_evaluado)/100 
+    return similarity
+  else:
+    return difflib.SequenceMatcher(None, s_buscado, s_evaluado).ratio()
+    
+ 
+ def get_variables_index(df_doc, dict_parameters, compare_function, treshold=0.85):
+
+  dict_variables = {}
+
+  for name_var in dict_parameters.keys():
+    # variable = df_variables[name_var].values
+    # variable = variable[~pd.isnull(variable)]
+    variable = dict_parameters[name_var]
+
+    probs = []
+    keys = []
+    coordinates = []
+
+    for var in variable:
+      matriz_prob = df_doc.applymap(lambda x: compare_function(x,var))
+
+      coordinate = np.unravel_index(np.argmax(matriz_prob, axis=None), matriz_prob.shape)
+      prob = matriz_prob.values[coordinate[0]][coordinate[1]]
+      key = df_doc.values[coordinate[0]][coordinate[1]]
+
+      probs.append(prob)
+      keys.append(key)
+      coordinates.append(coordinate)
+
+    argmax = np.argmax(probs)  
+    coordinate_max = coordinates[argmax]
+    prob_max = probs[argmax]
+    key_max = keys[argmax]
+    
+    if prob_max >= treshold:
+      dict_variables[name_var] = [int(coordinate_max[0]),int(coordinate_max[1])]
+
+  # with open(name_file, 'w') as file:
+    # json.dump(dict_variables, file)
+
+  return dict_variables
+  
+  
+  
+def get_dict_vars_values(df, dict_vars, name_file="variables_indices.json"):
+  
+  _df = df.copy()
+
+  pos_row = np.argmax(_df.applymap(is_year).sum(1).values)
+  _df.columns = _df.iloc[pos_row]
+
+  years = [get_year(col) for col in _df.columns]
+ 
+  year = np.max(years)
+ 
+  col_year = _df.columns[np.argmax(years)]
+ 
+  dict_vars_values = {}
+
+  for key in dict_vars:
+    x, y = dict_vars[key]
+    value = _df[col_year][x]
+    # value = value.replace('.','')
+    # value = value.replace(',','.')
+    dict_vars_values[key] = value
+  
+  dict_vars_values["Fecha"] = int(year)
+
+  with open(name_file, 'w') as file:
+    json.dump(dict_vars_values, file)
+
+  return dict_vars_values
+
+
+def main():
+  with open('/content/drive/My Drive/HACKATHONBBVA_2020/variable_dictionary/variables.json', 'r') as j:
+    dict_parameters = json.load(j)
+
+  path_table = "/content/drive/My Drive/HACKATHONBBVA_2020/doc_to_table/image_Doc2_variables.csv"
+  path_save = "/content/drive/My Drive/HACKATHONBBVA_2020/final_values/"
+  df = pd.read_csv(path_table)
+  df = processing_text(df)
+
+  dict_variables = get_variables_index(df, dict_parameters, sequence_matcher_similarity)
+
+  dict_variables = get_dict_vars_values(df, dict_variables, name_file=path_save + path_table.split('/')[-1].split('.')[0] + '_final.json')
+  # dict_variables = get_dict_vars_values(df, dict_parameters, sequence_matcher_similarity, name_file=path_save + path_table.split('/')[-1].split('.')[0] + '_final.json')
+
