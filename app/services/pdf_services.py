3a4278b286201def862e6f35468591750b29a1f7
Joel Cabrera <jjcabrerarios@gmail.com>
2020-10-17 08:49:35 -0500

    add blob img

--- app/services/pdf_services.py
+++ app/services/pdf_services.py
@@ -5,9 +5,11 @@ def processDocument():
 
     # test
     document = 'doc_example.png'
+    with open('document') as f:
+        image_blob = f.read()
 
     clientTextract = boto3.client('textract', region_name='us-east-1')
-    response = clientTextract.detect_document_text(Document={'Bytes':document})
+    response = clientTextract.detect_document_text(Document={'Bytes':image_blob})
     print('Response textract', response)
     return 'uwu'
 
