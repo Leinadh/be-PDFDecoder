678f08872fa636198eb2d44ad09d3d891f3f78bf
Joel Cabrera <jjcabrerarios@gmail.com>
2020-10-17 09:19:57 -0500

    test textract

--- app/services/pdf_services.py
+++ app/services/pdf_services.py
@@ -1,11 +1,13 @@
 import boto3
+import os
 
 def processDocument():
     # convert pdf to image is necessary
 
     # test
+    # print('wd:: ', os.getcwd())
     document = 'doc_example.png'
-    with open('document') as f:
+    with open(document, 'rb') as f:
         image_blob = f.read()
 
     clientTextract = boto3.client('textract', region_name='us-east-1')
